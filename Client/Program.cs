8043f714aebc8c8957ac09401cea3b2a7d8b4f69
Stronauta <samircastillop@gmail.com>
2024-12-03 12:01:50 -0400

    Agregando graficos y mejorando logica

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,16 +1,15 @@
-using Empresa.Client.Services;
 using Empresaa.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddScoped<DataService>();
+builder.Services.AddRadzenComponents();
 
 
 await builder.Build().RunAsync();
